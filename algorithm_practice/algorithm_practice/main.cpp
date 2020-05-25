88912c589311aa74b4a0193b72bf315375d2cc1f
chenhao <chenhao@chenhaodeMacBook-Pro.local>
2020-05-25 09:37:48 +0800

    algorithm 2

--- algorithm_practice/algorithm_practice/main.cpp
+++ algorithm_practice/algorithm_practice/main.cpp
@@ -73,10 +73,21 @@ void PrintQueue(QueueList *pList)
     printf("\n");
 }
 
+void TreePrint(BiTree T)
+{
+    if(T == NULL){
+        return;
+    }
+
+    TreePrint(T->plchild);
+    TreePrint(T->prchild);
+    printf("%d",T->data);
+}
+
 //6.设一颗二叉树中各节点的值互不相同，其先序遍历序列和中序遍历序列分别存于两个一维数组A[1...n]和B[1...n]中，
 //实现建立该二叉树的二叉链表
-int A[] = {1,2,3,4,5};
-int B[] = {3,2,1,5,4};
+int A[] = {1,2,3,4,5,6};
+int B[] = {3,2,1,5,4,6};
 
 BiTree CreateTree(int A[], int B[], int f1, int r1, int f2, int r2)
 {
@@ -114,22 +125,14 @@ bool IsTreeComplete(BiTree T)
     QueueList *pListQueue = InitQueue();
     BiTree p = T;
     EnQueue(pListQueue, p);
-    int count = 0;
     while(!IsQueueEmpty(pListQueue)){
         PrintQueue(pListQueue);
         p = DelQueue(pListQueue);
-        printf("count:%d\n",count);
         if(p){
-            printf("data:%d\n",p->data);
             EnQueue(pListQueue, p->plchild);
-            if(p->plchild)
-                printf("plchild:%d\n",p->plchild->data);
             EnQueue(pListQueue, p->prchild);
-            if(p->prchild)
-                printf("prchild:%d\n",p->prchild->data);
         }
         else{
-            printf("data:null");
             while(!IsQueueEmpty(pListQueue)){
                 if(DelQueue(pListQueue))
                     return 0;
@@ -140,25 +143,135 @@ bool IsTreeComplete(BiTree T)
     return 1;
 }
 
-void TreePrint(BiTree T)
+//8. 计算一棵给定二叉树的所有双分支节点数
+int CountDoubleBranchNode(BiTree T)
+{
+    if(T == NULL)
+        return 0;
+
+    QueueList *pListQueue = InitQueue();
+    BiTree p = T;
+    EnQueue(pListQueue, p);
+    int count = 0;
+    
+    while(!IsQueueEmpty(pListQueue)){
+        p = DelQueue(pListQueue);
+        if(p->plchild)
+            EnQueue(pListQueue, p->plchild);
+        if(p->prchild)
+            EnQueue(pListQueue, p->prchild);
+        if(p->plchild && p->prchild)
+            count++;
+    }
+    
+    return count;
+}
+
+//9. 编写一个把树B所有结点的左右子树进行交换的函数
+void SwitchTreeNode(BiTree T)
+{
+    if(T == NULL)
+        return;
+    
+    SwitchTreeNode(T->plchild);
+    SwitchTreeNode(T->prchild);
+    BiTree p = T->plchild;
+    T->plchild = T->prchild;
+    T->prchild = p;
+}
+
+//10.求先序遍历序列中第k（1<=k<=二叉树结点个数）个结点的值
+int count = 0;
+int value = 0;
+void GetPreOrderValue(BiTree T, int num)
+{
+    if(T == NULL)
+        return;
+    count++;
+    if(count == num)
+        value = T->data;
+    GetPreOrderValue(T->plchild,num);
+    GetPreOrderValue(T->prchild,num);
+    printf("value:%d\n", value);
+}
+
+//11.对于树中每个元素值为x的结点，删去以它为根的子树，并释放相应的空间
+void FreeTree(BiTree T)
+{
+    if(T == NULL)
+        return;
+    FreeTree(T->plchild);
+    FreeTree(T->prchild);
+    free(T);
+}
+void FreeValueX(BiTree T, int x)
 {
     if(T == NULL)
         return;
 
-    TreePrint(T->plchild);
-    TreePrint(T->prchild);
-    printf("node%d\n",T->data);
+    QueueList *pListQueue = InitQueue();
+    BiTree p = T;
+    EnQueue(pListQueue, p);
+    
+    while(!IsQueueEmpty(pListQueue)){
+        p = DelQueue(pListQueue);
+        if(p->plchild){
+            if(p->plchild->data == x){
+                printf("free:");
+                TreePrint(p);
+                printf("\n");
+                FreeTree(p->plchild);
+                p->plchild = NULL;
+            }
+            else
+                EnQueue(pListQueue, p->plchild);
+        }
+        if(p->prchild){
+            if(p->prchild->data == x){
+                printf("free:");
+                TreePrint(p);
+                printf("\n");
+                FreeTree(p->prchild);
+                p->prchild = NULL;
+            }
+            else
+                EnQueue(pListQueue, p->prchild);
+        }
+    }
+    
 }
+
+//12.打印二叉树中值为x的结点的所有祖先，假设值为x的结点不止一个
+
+
 
 int main()
 {
     //6
-    BiTree p = CreateTree(A, B, 0, 4, 0, 4);
+    BiTree p = CreateTree(A, B, 0, 5, 0, 5);
     //TreePrint(p);
 
     //7
     int ret = IsTreeComplete(p);
     printf("ret:%d\n",ret);
+    
+    //8
+    ret = CountDoubleBranchNode(p);
+    printf("double branch nodes num:%d\n",ret);
+    
+    //9
+    //TreePrint(p);
+    //SwitchTreeNode(p);
+    //TreePrint(p);
 
+    //10
+    GetPreOrderValue(p,6);
+    
+    //11
+    TreePrint(p);
+    printf("\n");
+    FreeValueX(p, 4);
+    TreePrint(p);
+    
     return 0;
 }
